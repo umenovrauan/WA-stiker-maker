ca68f0c50879be4debb78d92a0a065c49bfab57f
umenovrauan <umenovrauan@gmail.com>
2023-12-17 15:11:22 +0600

    Initial Commit

---
+++ WA stiker maker/WA_stiker_makerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WA_stiker_makerApp.swift
+//  WA stiker maker
+//
+//  Created by Rauan Umenov on 17.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct WA_stiker_makerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
